a9811ce30fba009f751040fd585feff0cb3af820
HY0109 <h098s02602@chunchon.gwe.hs.kr>
2025-11-18 12:24:45 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      },
 labels[1]: {
        "texts": ["키스톤의 힘으로 포켓몬이 진화함", "메가진화의 영향으로 외형이 변화함", "포켓몬의 능력치가 상승함"],
-       "images": ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fnamu.wiki%2Fw%2F%25EC%2595%2584%25EC%25BF%25A0%25EC%258A%25A4%25ED%2583%2580&psig=AOvVaw1jrGlzsNyZSA7ujaGN_gKW&ust=1763522041871000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMj88tjd-pADFQAAAAAdAAAAABAE", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fnamu.wiki%2Fw%2F%25EC%259D%25B4%25EC%2596%25B4%25EB%25A1%25AD&psig=AOvVaw0GOOV8iGzC3NG7wF9UL_-l&ust=1763522150323000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLCS-Yze-pADFQAAAAAdAAAAABAE"],
+       "images": ["https://www.google.com/imgres?q=%EA%B0%80%EB%94%94%EC%95%88%20%EB%A9%94%EA%B0%80%EC%A7%84%ED%99%94&imgurl=https%3A%2F%2Fblogthumb.pstatic.net%2F20160623_222%2Fjhshin329_1466670991640qGF2t_PNG%2F282Gardevoir-Mega.png%3Ftype%3Dw2&imgrefurl=https%3A%2F%2Fblog.naver.com%2Fjhshin329%2F220744167220%3FviewType%3Dpc&docid=iYm1fQq2zfr6uM&tbnid=kXco1ADoU7jr8M&vet=12ahUKEwjz_rT63_qQAxXRk1YBHU1kAHQQM3oECB8QAA..i&w=300&h=300&hcb=2&ved=2ahUKEwjz_rT63_qQAxXRk1YBHU1kAHQQM3oECB8QAA", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fnamu.wiki%2Fw%2F%25EC%259D%25B4%25EC%2596%25B4%25EB%25A1%25AD&psig=AOvVaw0GOOV8iGzC3NG7wF9UL_-l&ust=1763522150323000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLCS-Yze-pADFQAAAAAdAAAAABAE"],
        "videos": ["https://www.youtube.com/watch?v=gHXfCWGZWNs"] }
 }
 # ======================
